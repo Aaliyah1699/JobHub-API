b273e5bcea0080784d9fcf6432497abb4a1f0799
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-21 16:05:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Jobs API
+
+**User Model**
+
+Email Validation Regex
+
+```regex
+/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+```
+
+**Register User**
+
+- Validate - name, email, password - with Mongoose
+- Hash Password (with bcryptjs)
+- Save User
+- Generate Token
+- Send Response with Token
+
+**Login User**
+
+- Validate - email, password 
+- Find User
+- Compare Passwords
+- generate Token
+- Send Response with Token
+
+ **Mongoose Errors**
+
+- Validation Errors
+- Duplicate (Email)
+- Cast Error
+
+**Security**
+
+- helmet
+- cors
+- express-rate-limit
---
+++ middleware/error-handler.js
@@ -0,0 +1,35 @@
+const { StatusCodes } = require("http-status-codes");
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+    let customError = {
+        // set default
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || "something went wrong try again later",
+    };
+    // validation error handler
+    if (err.name === "ValidationError") {
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(", ");
+
+        customError.statusCode = 400;
+    }
+
+    // duplicate error handler
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate value entered for ${Object.keys(
+            err.keyValue
+        )} field, please chose another value`;
+        customError.statusCode = 400;
+    }
+
+    // cast error
+    if (err.name === "CastError") {
+        customError.msg = `No item found with id : ${err.value}`;
+        customError.statusCode = 404;
+    }
+
+    return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
